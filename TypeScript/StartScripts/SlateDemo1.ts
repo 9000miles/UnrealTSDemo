0308c8a0134a297ccdabb60043fc614cfbcf4a85
lizijun <1655753014@qq.com>
2024-04-30 08:44:42 +0800

    Widget 绑定函数OK

--- TypeScript/StartScripts/SlateDemo1.ts
+++ TypeScript/StartScripts/SlateDemo1.ts
@@ -21,15 +21,43 @@ function Func1() {
     console.log(">>>>>>>>>> AAA <<<<<<<<<<", text)
 }
 
-function Func2() {
-    function getColor() {
-        return "rgba(128,19,115,0.3)"
-    }
-
+function Func1_1() {
     const widget: TSharedPtr<STextBlock> = STextBlock.SNew({
             Text: () => {
-                return "sss"
+                return "ss"
             },
+            ColorAndOpacity: "rgba(93,197,19,0.92)"
+        },
+        __filename)
+    GameInstance.SetTestWidget(widget)
+
+
+    const text = widget.Get().GetText()
+    console.log(">>>>>>>>>> AAA <<<<<<<<<<", text)
+}
+
+function Func2() {
+    class AA {
+        classFunc() {
+            return String(UE.KismetMathLibrary.RandomInteger(60))
+        }
+    }
+
+    function getColor() {
+        return "rgba(128,19,115,0.89)"
+    }
+
+    function getText() {
+        return String(UE.KismetMathLibrary.RandomFloat())
+        return "this is getText"
+    }
+
+    const aa = new AA()
+
+    const widget: TSharedPtr<STextBlock> = STextBlock.SNew({
+            Text: getText,
+            // Text: aa.classFunc,
+            // Text: () => null,
             ColorAndOpacity: getColor
         },
         __filename)
@@ -50,15 +78,7 @@ function Func3() {
 /**
  * ================= 测试 =================
  **/
-const Functions = [
-    Func1,
-    Func2,
-    Func3,
-]
-console.log(Func1.name)
-const TestIndex = 0;
-const testFunc = Func1
-testFunc()
+Func2()
 
 
 
