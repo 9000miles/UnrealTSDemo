b7362df110635c399f5465e7f9ea0f50b64395a6
lizijun <1655753014@qq.com>
2024-05-05 15:50:20 +0800

    ts 继承C++类型，并覆写函数

--- TypeScript/StartScripts/SlateDemo0.ts
+++ TypeScript/StartScripts/SlateDemo0.ts
@@ -1,40 +1,105 @@
-import {SButton, STextBlock, SWidgetBuilder, TSharedPtr} from "cpp"
 import * as cpp from "cpp";
 import * as UE from "ue";
 import * as puerts from "puerts";
-
-// const args: STextBlock.Arguments = {
-//     Text: "AAA"
-// }
-// const stbPtr0: TSharedPtr<STextBlock> = SNew(STextBlock, {
-//     Text: "AAA"
-// })
-// const text0 = stbPtr0.Get().GetText()
-// console.log(text0)
-
+import {BaseClass, ChildClass, Script_ChildClass} from "cpp";
 
 const GameInstance: UE.TestPuertsSlateGameInstance = puerts.argv.getByName("GameInstance") as UE.TestPuertsSlateGameInstance
 
-const stbPtr: TSharedPtr<STextBlock> = SWidgetBuilder.Create(STextBlock.WidgetType, {
-    args: {
-        Text: "AAA"
-    },
-    cp1: "bb",
-    xx: 355
-})
 
-const text = stbPtr.Get().GetText()
-console.log(">>>>>>>>>> AAA <<<<<<<<<<", text)
+let aa = new cpp.ChildClass();
 
-const btn = SWidgetBuilder.CreateButton(SButton.WidgetType, {
-    args: {
-        Text: "my name is button",
-        OnClicked: () => {
-            console.log("button clicked !!!")
 
-            GameInstance.SetTestWidget(stbPtr)
-        }
+class TS_Class1 extends ChildClass {
+    Tick(p0: number) {
+        super.Tick(p0);
+        console.log("TS_Class1::Tick")
     }
-})
+}
 
-GameInstance.SetTestWidget(btn)
+aa = new TS_Class1()
+
+class tt {
+    Tick() {
+        // super.Tick()
+    }
+}
+
+class TS_Class2 extends ChildClass {
+    age: number
+    t: tt
+
+    constructor() {
+        super()
+        // super({});
+        // this.Register(this)
+        this.age = 456
+        this.t = new tt()
+    }
+
+    Tick(p0: number) {
+        super.Tick(p0);
+        console.log("TS_Class2222::Tick", p0, this.age)
+
+        // const v = new tt()
+        // const ab = Object.getOwnPropertyDescriptor(v, "Tick")
+        // console.log(ab)
+        // console.log("TS_Class2222::Tick", p0, this.age)
+    }
+}
+
+
+class TS_Class3 extends Script_ChildClass {
+    age: number
+    t: tt
+
+    constructor() {
+        super({});
+        this.Register(this)
+        this.age = 456
+        this.t = new tt()
+    }
+
+    Tick(p0: number) {
+        // super.Tick(p0);
+        this.Super.Tick(p0);
+        // this.constructor.prototype.Tick.call(this, p0)
+        console.log("TS_Class2222::Tick", p0, this.age)
+
+        // const v = new tt()
+        // const ab = Object.getOwnPropertyDescriptor(v, "Tick")
+        // console.log(ab)
+        // console.log("TS_Class2222::Tick", p0, this.age)
+    }
+}
+
+aa = new TS_Class3()
+
+
+GameInstance.SetScriptClass(aa)
+
+
+class A {
+    Tick(d: number) {
+        console.log("A:Tick")
+    }
+}
+
+
+class B extends A {
+    Tick(d: number) {
+        super.Tick(d)
+        console.log("B:Tick")
+    }
+}
+
+
+class C extends B {
+    Tick(d: number) {
+        // A.prototype.Tick.call(this, d)
+        super.Tick(d)
+        console.log("C:Tick")
+    }
+}
+
+const f = new C()
+f.Tick(2)
